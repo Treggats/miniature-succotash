cf4279df8514f338846038cfd34e475bb44a4313
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-05-31 09:08:32 +0200

    added SearchBar component to App
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component
 {
     render()
     {
         return (
-            <div className="greeting">
-                <p className="greeting-text">Hello World</p>
-            </div>
+            <SearchBar />
         );
     }
 }
