d4523d7ec57f68c8e712c0ee48abc2fdfe125171
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-01 22:45:09 +0200

    cleaned up the App container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,77 +1,38 @@
 import React from 'react';
 import Modal from "react-modal";
-import axios from 'axios';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import SearchBar from '../components/SearchBar';
-import GifList from '../components/GifList';
-import GifModal from '../components/GifModal';
+import * as Actions from '../actions';
 import '../styles/app.css';
 
 class App extends React.Component
 {
-    constructor()
-    {
-        super();
-        this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalIsOpen: false
-        };
-
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
-
     componentWillMount()
     {
         Modal.setAppElement('body');
     }
 
-    openModal = (gif) => {
-        this.setState({
-            modalIsOpen: true,
-            selectedGif: gif
-        });
-    };
-
-    closeModal = () => {
-        this.setState({
-            modelIsOpen: false,
-            selectedGif: null
-        });
-    };
-
-    handleTermChange = (term) => {
-        const apiKey = 'dc6zaTOxFJmzC';
-        const limit = 10;
-        const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${apiKey}&limit=${limit}`;
-
-        axios.get(url).then(response => {
-            this.setState({
-                gifs: response.data.data
-            });
-        }).catch(error => {
-            if ('response' in error) {
-                console.log(error.response.data);
-            }
-        });
-    };
-
     render()
     {
         return (
             <div>
-                <SearchBar onTermChange={this.handleTermChange} />
-                <GifList
-                    gifs={this.state.gifs}
-                    onGifSelect={selectedGif => this.openModal(selectedGif)}
-                />
-                <GifModal
-                    modalIsOpen={this.state.modalIsOpen}
-                    selectedGif={this.state.selectedGif}
-                    onRequestClose={ () => this.closeModal()}
-                />
+                <SearchBar onTermChange={this.props.actions.requestGifs} />
             </div>
         );
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        gifs: state.gifs
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(Actions, dispatch)
+    };
+}
+    
+export default connect(mapStateToProps, mapDispatchToProps)(App);
