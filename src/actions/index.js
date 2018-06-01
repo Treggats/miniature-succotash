ce5b7fad2b9b7627cc111846b534dcf892f6d776
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-01 22:44:23 +0200

    added action to fetch the gifs

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const REQUEST_GIFS = 'REQUEST_GIFS';
+
+const apiKey = 'dc6zaTOxFJmzC';
+const limit = 10;
+const API_URL = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=${limit}&q=`;
+
+export function requestGifs(term = null) {
+    const data = axios.get(API_URL + term.replace(/\s/g, '+'));
+
+    return {
+        type: REQUEST_GIFS,
+        payload: data.data
+    }
+}
