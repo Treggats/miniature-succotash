02f3a9366355d28b536440a481fccb8dff64b644
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-01 13:45:55 +0200

    moved App class to it's container
    implemented store

--- src/index.js
+++ src/index.js
@@ -1,78 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from "react-modal";
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
-import GifList from './components/GifList';
-import GifModal from './components/GifModal';
-import './styles/app.css';
+import App from './containers/App';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 
-class App extends React.Component
-{
-    constructor()
-    {
-        super();
-        this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalIsOpen: false
-        };
+const store = configureStore();
 
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
-
-    componentWillMount()
-    {
-        Modal.setAppElement('body');
-    }
-
-    openModal = (gif) => {
-        this.setState({
-            modalIsOpen: true,
-            selectedGif: gif
-        });
-    };
-
-    closeModal = () => {
-        this.setState({
-            modelIsOpen: false,
-            selectedGif: null
-        });
-    };
-
-    handleTermChange = (term) => {
-        const apiKey = 'dc6zaTOxFJmzC';
-        const limit = 10;
-        const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${apiKey}&limit=${limit}`;
-
-        axios.get(url).then(response => {
-            this.setState({
-                gifs: response.data.data
-            });
-        }).catch(error => {
-            if ('response' in error) {
-                console.log(error.response.data);
-            }
-        });
-    };
-
-    render()
-    {
-        return (
-            <div>
-                <SearchBar onTermChange={this.handleTermChange} />
-                <GifList
-                    gifs={this.state.gifs}
-                    onGifSelect={selectedGif => this.openModal(selectedGif)}
-                />
-                <GifModal
-                    modalIsOpen={this.state.modalIsOpen}
-                    selectedGif={this.state.selectedGif}
-                    onRequestClose={ () => this.closeModal()}
-                />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('app'));
