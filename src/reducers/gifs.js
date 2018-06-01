3cdfa366c7e5da20e3bb39a8ea5a8e3857092199
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-01 13:44:30 +0200

    added dummy gif reducer

---
+++ src/reducers/gifs.js
@@ -0,0 +1,16 @@
+export default () => {
+    return [
+        {
+            id: 1,
+            url: 'https://dummyimage.com/300x300/123/ccc.png'
+        },
+        {
+            id: 2,
+            url: 'https://dummyimage.com/300x300/123/ccc.png'
+        },
+        {
+            id: 3,
+            url: 'https://dummyimage.com/300x300/123/ccc.png'
+        },
+    ];
+}
