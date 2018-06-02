7b2699a5a208479c18f59136192889f14875ccf0
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-02 11:08:23 +0200

    replaced dummy gifs with an implementation

--- src/reducers/gifs.js
+++ src/reducers/gifs.js
@@ -1,16 +1,17 @@
-export default () => {
-    return [
-        {
-            id: 1,
-            url: 'https://dummyimage.com/300x300/123/ccc.png'
-        },
-        {
-            id: 2,
-            url: 'https://dummyimage.com/300x300/123/ccc.png'
-        },
-        {
-            id: 3,
-            url: 'https://dummyimage.com/300x300/123/ccc.png'
-        },
-    ];
+import {REQUEST_GIFS} from '../actions';
+
+const initialState = {
+    data: []
+};
+
+export default function gifs(state = initialState, action) {
+    switch(action.type) {
+        case REQUEST_GIFS:
+            return {
+                ...state, data: action.payload.data.data
+            };
+        
+        default:
+            return state;
+    }
 }
