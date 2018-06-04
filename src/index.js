a4202197f28daf96c9465ce35dee7060652e8caa
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-04 20:39:02 +0200

    using a new App component
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './components/App';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 
