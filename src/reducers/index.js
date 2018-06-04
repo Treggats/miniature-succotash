b3a8d648f6cd134e0337e0632e3a275ac7caa645
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-04 20:45:12 +0200

    prepare for router/implement history
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import GifReducer from './gifs';
 import ModalReducer from './modal';
+import {routerReducer} from 'react-router-redux';
 
 const rootReducer = combineReducers({
     gifs: GifReducer,
-    modal: ModalReducer
+    modal: ModalReducer,
+    router: routerReducer
 });
 
 export default rootReducer;
