a45429bc3fdbc8dbdd700122bc77bed2bc961910
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-06 20:05:22 +0200

    using the redux form in the root reducer
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 import GifReducer from './gifs';
 import ModalReducer from './modal';
 import {routerReducer} from 'react-router-redux';
+import {reducer as FormReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     gifs: GifReducer,
     modal: ModalReducer,
     router: routerReducer
