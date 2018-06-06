6b5a0c0dd42a7311235eb10f2a3b0faa92f83d63
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-06 19:39:41 +0200

    implemented App component

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {ConnectedRouter} from 'react-router-redux';
+import {Route} from 'react-router-dom';
+import {history} from '../store/configureStore';
+
+import Header from '../containers/Header';
+import Home from '../containers/Home';
+import Signup from '../containers/Signup';
+import Login from '../containers/Login';
+import Favorites from '../containers/Favorites';
+
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <ConnectedRouter history={history}>
+                <div>
+                    <Header />
+
+                    <div className="container">
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/signup" component={Signup} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/favorites" component={Favorites} />
+                    </div>
+                </div>
+            </ConnectedRouter>
+        );
+    }
+}
+
+export default App;
