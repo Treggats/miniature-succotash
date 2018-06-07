72ab711aa5b67376079ee87780e546c5cad34d8c
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-06-07 07:57:26 +0200

    created the login form
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class Login extends React.Component
 {
+    handleFormSubmit = (values) => {
+        console.log(values);
+    };
+
     render()
     {
-        return <div>Login</div>;
+        return (
+            <div className="container">
+                <div className="col-md-6 col-md-offset-3">
+                    <h2 className="text-center">Log In</h2>
+                    <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
+                        <fieldset className="form-group">
+                            <label for="email">E-mail</label>
+                            <Field name="email" component="input" className="form-control" type="text" placeholder="email" />
+                        </fieldset>
+
+                        <fieldset className="form-group">
+                            <label for="password">Password</label>
+                            <Field name="password" component="input" className="form-control" type="text" placeholder="password" />
+                        </fieldset>
+
+                        <button action="submit" className="btn btn-primary">Sign In</button>
+                    </form>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Login;
+export default reduxForm({
+    form: 'login'
+})(Login);
