77dbc9a978eb2ce81d3eb58c899f9739eb09c03a
Tonko Mulder <tonko@tonkomulder.nl>
2018-06-08 07:59:45 +0000

    added validation function to Signup

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+
+const validate = values => {
+    const errors = {};
+
+    if (!values.email) {
+        errors.email = "Please enter an email.";
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Invalid email address";
+    }
+
+    if (!values.password) {
+        errors.password = "Please enter a password.";
+    }
+    if (!values.passwordConfirmation) {
+        errors.passwordConfirmation = "Please enter a password confirmation.";
+    }
+    if (values.password !== values.passwordConfirmation) {
+        errors.password = "Passwords do not match;";
+    }
+
+    return errors;
+};
 
 class Signup extends React.Component
 {
