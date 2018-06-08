9f26676ca91e7c216771850f67a93eadb76d17f4
Tonko Mulder <tonko@tonkomulder.nl>
2018-06-08 08:43:37 +0000

    added auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
+import AuthReducer from './auth';
 import GifReducer from './gifs';
 import ModalReducer from './modal';
 import {routerReducer} from 'react-router-redux';
 import {reducer as FormReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
+    auth: AuthReducer,
     form: FormReducer,
     gifs: GifReducer,
     modal: ModalReducer,
